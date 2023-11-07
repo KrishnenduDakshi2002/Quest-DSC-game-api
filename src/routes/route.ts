ca092cb89961fc4dbadb69696692599a71d960ea
KrishnenduDakshi2002 <kdakshi2020@gmail.com>
2023-11-07 20:46:42 +0530

    initial route

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express';
 
 const router = Router();
 router.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Hello World, I am krishnendu Dakshi, HIIII' });
+  res.json({ message: 'Hello World, This is GDSC quest' });
 });
 
 export default router;
