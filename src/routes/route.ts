9804c2b8a96e7c08dc9b3129a839c779ce59a278
KrishnenduDakshi2002 <kdakshi2020@gmail.com>
2023-11-07 20:41:01 +0530

    changes in route

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express';
 
 const router = Router();
 router.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Hello World, I am krishnendu Dakshi' });
+  res.json({ message: 'Hello World, I am krishnendu Dakshi, HIIII' });
 });
 
 export default router;
